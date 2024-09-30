cd3a1ca4b1243ff24dbdf3b43cc76a52cb12c932
Roksolyanka <roksolyanakushnir@gmail.com>
2024-09-30 14:42:16 +0300

    add theme

---
+++ src/theme.tsx
@@ -0,0 +1,31 @@
+const theme = {
+  colors: {
+    browns: [
+      '#f9f6f3',
+      '#f0ebe4',
+      '#e1d4c7',
+      '#cdb8a4',
+      '#bea08a',
+      '#a97f66',
+      '#825b4c',
+      '#6a4b42',
+      '#573e37',
+      '#2e201c',
+    ],
+    blue: ['#aecde7'],
+    red: ['#8b2605'],
+    backdropColor: 'rgba(46, 47, 66, 0.4)',
+    boxShadow: '13px 13px 24px 1px rgba(46,32,28,1);',
+    boxShadowAccent: '13px 13px 13px 3px rgba(174,205,231,1)',
+  },
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
+};
+
+export default theme;
